3b19f87578276c925024f856cdac7eea9eda80fa
Monika Nanda <monika@MacBook-Pro.local>
2020-08-11 03:22:19 -0400

    Update still does not work

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://murmuring-cliffs-14664.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
